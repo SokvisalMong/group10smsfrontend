bea5bff40448ee9ee2fd4898f5958a481517d493
vsea111 <107669400+vsea111@users.noreply.github.com>
2022-07-15 16:54:36 +0700

    Allowing user to see which department is being edited

--- src/components/departmentcomponents/editDepartment.js
+++ src/components/departmentcomponents/editDepartment.js
@@ -64,7 +64,7 @@ export default function Edit() {
  // This following section will display the form that takes input from the user to update the data.
  return (
    <div>
-     <h3>Update Department</h3>
+     <h3>Update Department {form.dept_id} : {form.dept_name}</h3>
      <form onSubmit={onSubmit}>
        <div className="form-group">
          <label htmlFor="dept_id">Department ID: </label>
